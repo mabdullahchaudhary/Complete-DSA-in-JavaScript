9423fb4d15d5dc7191bd30787b7ac81a381cd1a3
Abdullah <mhabdullah000@gmail.com>
2026-06-13 16:12:44 +0500

    Second Video

---
+++ Basics/array_data_Structure.ts
@@ -0,0 +1,14 @@
+// You definately say's you know about the arrays but you don't
+
+// if yes then tell me is this array or not ?
+//                 let A=[];
+// cuz in js there is no types
+
+/*
+  Arrays is a contigious memory location which is used to store the data of same type and it is also a data structure which is used to store the data of same type in a contigious memory location.
+
+    be like [0, ................................, n-1] where n is the size of the array and 0 is the starting index of the array and n-1 is the ending index of the array.
+
+    
+
+*/
